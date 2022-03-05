688457476d607f10202ddbcb9335c97bb9408451
byamane <byamane21@gmail.com>
2022-03-05 17:08:22 -0600

    add new review view

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,7 +1,17 @@
 import { Review } from "../models/review.js"
+import { Game } from "../models/game.js"
 
+function newReview(req, res){
+  res.render('reviews/new', {
+    title: "Add Review"
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/games/:id")
+  })
+}
 
 
 export {
-  
+  newReview as new,
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,12 @@
 import { Router } from "express"
 import * as reviewsCtrl from "../controllers/reviews.js"
+import { isLoggedIn } from "../middleware/middleware.js"
+
 const router = Router()
 
 // GET - localhost:3000/reviews
+router.get('/new', isLoggedIn, reviewsCtrl.new)
+
+export {
+  router
+}
