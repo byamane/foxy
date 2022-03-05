c58d55ba16bc49af7dc25fc9c5fde64318790f37
byamane <byamane21@gmail.com>
2022-03-04 20:59:51 -0600

    add game index route & initial view

--- routes/games.js
+++ routes/games.js
@@ -1,3 +1,12 @@
 import { Router } from 'express'
+import * as gamesCtrl from "../controllers/games.js"
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
+
+// GET - localhost:3000/games
+router.get('/', isLoggedIn, gamesCtrl.index)
+
+export {
+  router
+}
