512a75c3cdd345ec79243798c97477bb388338d4
byamane <byamane21@gmail.com>
2022-03-04 20:21:44 -0600

    add gameSchema to model

---
+++ models/game.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const gameSchema = new Schema ({
+  name: {type: String, required: true},
+  genre: {type: String, required: true},
+  esrbRating: {type: String, required: true},
+  releaseYear: {type: Number, required: true},
+  //coverImage: {type: String, required: true}
+  addedBy: {type: Schema.Types.ObjectId, ref: "Profile"},
+  allReviews: {type: Schema.Types.ObjectId, ref: "All Reviews"}
+})
+
+const Game = mongoose.model("Game", gameSchema)
+
+export {
+  Game
+}
