eca37c88185cd3bf3440884bbc0517056c1d2e46
byamane <byamane21@gmail.com>
2022-03-07 17:26:28 -0600

    add show profile functionality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -14,6 +14,26 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .then(profile => {
+    Profile.findById(req.user.profile._id)
+    .then(self => {
+      const isSelf = self._id.equals(profile._id)
+      res.render("profiles/show", {
+        title: `${profile.name}'s profile`,
+        profile,
+        isSelf
+      })
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
 export {
   index,
+  show,
 }
