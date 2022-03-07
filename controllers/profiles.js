15cb9056408aaa52765df45d4233233beb8f962a
byamane <byamane21@gmail.com>
2022-03-07 17:11:18 -0600

    add profiles index view & styling

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,2 +1,19 @@
 import { Profile } from "../models/profile.js"
 
+function index(req, res) {
+  Profile.find({})
+  .then(profiles => {
+    res.render('profiles/index', {
+      profiles,
+      title: "Users"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
+}
+
+export {
+  index,
+}
---
+++ routes/profiles.js
@@ -0,0 +1,12 @@
+import { Router } from "express"
+import * as profilesCtrl from "../controllers/profiles.js"
+import { isLoggedIn } from '../middleware/middleware.js'
+
+const router = Router()
+
+// GET - localhost:3000/profiles
+router.get("/", isLoggedIn, profilesCtrl.index)
+
+export {
+  router
+}
