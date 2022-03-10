ac2e85f430ec34eba084214ba812e4279ac998f5
byamane <byamane21@gmail.com>
2022-03-09 18:43:15 -0600

    add initial responsive styling to nav & games index

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const main = document.getElementsByClassName('content-push')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-  console.log("hi")
+  main.classList.toggle('active')
 })
